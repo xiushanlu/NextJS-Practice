7290fe2b3cf234efa7075a0b5fc628258f2f629f
Xiushan Lu <125161791+xiushanlu@users.noreply.github.com>
2025-07-14 23:21:26 -0700

    Disable some ESLint rules

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from 'next';
 
-import type { NextConfig } from 'next'
- 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
+  experimental: {
+    ppr: 'incremental'
+  }
+};
 
 export default nextConfig;
