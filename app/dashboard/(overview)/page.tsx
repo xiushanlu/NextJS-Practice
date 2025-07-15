9a7d983b3394773f39e4115f015ded7a25b53860
Xiushan Lu <125161791+xiushanlu@users.noreply.github.com>
2025-07-14 20:29:15 -0700

    Finish NextJS Course

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default async function Page() {
     const cardData = await fetchCardData;
